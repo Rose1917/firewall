c10c3aa99daf836f35c683fe2e05baaa0b592005
march1917 <renyanjie01@gmail.com>
2021-04-04 11:28:41 +0800

    remove the obj files and added a ignore file

--- .hello.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/march1917/Projects/firewall/hello.mod := { echo  /home/march1917/Projects/firewall/hello.o; echo; } > /home/march1917/Projects/firewall/hello.mod
--- .kernel.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/march1917/Projects/firewall/kernel.mod := { echo  /home/march1917/Projects/firewall/kernel.o; echo; } > /home/march1917/Projects/firewall/kernel.mod
